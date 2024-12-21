7ae4a11ae63b9a7e661017b1b6d1201769273493
miss-jane-osa <jane_i@hotmail.co.uk>
2024-12-21 16:51:14 +0000

    Added basic form handling with HTML and Javascript and added Typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+function generateQuote(event) {
+  event.preventDefault();
 
+  new Typewriter("#quote", {
+    strings: ["Insert quote here"],
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+let quoteFormElement = document.querySelector("#quote-generator-form");
+quoteFormElement.addEventListener("submit", generateQuote);
