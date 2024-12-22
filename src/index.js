0c9c2ceefeb75d5a5191882c5c2f026255c58b8b
miss-jane-osa <jane_i@hotmail.co.uk>
2024-12-22 18:16:07 +0000

    Added more information to the the context  of the AI Generator to get more precise results

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ function generateQuote(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "10tc88b5de8d7eoa39c8f2ea84faadb2";
   let context =
-    "You are a inspirational quote expert and you love to provide short inspirational quotes. Your mission is to generate short quotes in basic HTML and separate each line of the quote with a <br />. Do not include a title. Sign the quote with 'Your AI Assistant' inside a <strong> element at the end of the quote. Make sure to follow the user instructions";
+    "You are a inspirational quote expert and you love to provide short inspirational quotes. Your mission is to generate short quotes in basic HTML and separate each line of the quote with a <br />. Do not include a title. Do not include any markdown in the response. Do not include any quotation marks. Sign the quote with 'Your AI Assistant' inside a <strong> element at the end of the quote. Make sure to follow the user instructions";
   let prompt = `User instructions: Generate a quote about ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
